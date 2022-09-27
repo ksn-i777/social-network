82b8161f671e4f63653d8cfa0574753382f8045a
ksn-i777 <ksn_i777@mail.ru>
2022-09-27 21:20:50 +0300

    lesson 16

--- src/components/Profile/Posts/Post/Post.tsx
+++ src/components/Profile/Posts/Post/Post.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import s from "./Post.module.css"
 
-function Post() {
+type PostPropsType = {
+    message: string;
+    likes: number;
+}
+
+function Post(props: PostPropsType) {
     return (
         <div>
             <img className={s.ava} src="https://nash.live/img/forall/u/0/29/20190911154809-52881.jpg" alt="ava"/>
-            <span>post</span>
+            <span>{props.message}</span>
+            <div>likes: {props.likes}</div>
         </div>
 
     )
--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -7,11 +7,11 @@ function Posts() {
     return (
         <div>
             <NewPost />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post message={"Yaaahhhooo!"} likes={77}/>
+            <Post message={"Happy birthday to me!"} likes={100}/>
+            <Post message={"Have a good day!"} likes={60}/>
+            <Post message={"How are you?"} likes={30}/>
+            <Post message={"Hi. It is my first post"} likes={15}/>
         </div>
 
     )
