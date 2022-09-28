660c0a07e94b95495f2f3bce1264d59168971724
KSN <ksn_i777@mail.ru>
2022-09-28 11:32:42 +0300

    lesson 20

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import s from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 
 function Nav() {
     return(
         <nav className={s.nav}>
-            <div><a href="/profile" className={`${s.item} ${s.active}`}>Profile</a></div>
-            <div><a href="/messages" className={s.item}>Messages</a></div>
-            <div><a href="/news" className={s.item}>News</a></div>
-            <div><a href="/music" className={s.item}>Music</a></div>
-            <div><a href="/settings" className={s.item}>Settings</a></div>
+            <div><NavLink to="/profile" activeClassName={`${s.item} ${s.active}`}>Profile</NavLink></div>
+            <div><NavLink to="/messages" activeClassName={`${s.item} ${s.active}`}>Messages</NavLink></div>
+            <div><NavLink to="/news" activeClassName={`${s.item} ${s.active}`}>News</NavLink></div>
+            <div><NavLink to="/music" activeClassName={`${s.item} ${s.active}`}>Music</NavLink></div>
+            <div><NavLink to="/settings" activeClassName={`${s.item} ${s.active}`}>Settings</NavLink></div>
         </nav>
     )
 }
