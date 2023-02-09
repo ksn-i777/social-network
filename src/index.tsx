7c2fcac8bd4248630fea047376e7d04f88ff7f2e
ksn-i777 <ksn_i777@mail.ru>
2023-02-09 15:07:00 +0300

    lesson 95

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import {App} from './app/App'
-import {BrowserRouter} from "react-router-dom"
-import {store} from './store/store'
-import {Provider} from 'react-redux'
+import { App } from './app/App'
+import { HashRouter } from "react-router-dom"
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
     // компонента, обеспечивающая изменения контента при кликах по навигационному меню
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
-            <App/>
+            <App />
         </Provider>
-    </BrowserRouter> , document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
 );
